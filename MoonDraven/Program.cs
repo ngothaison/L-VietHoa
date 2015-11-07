19dc2c9a90f1e4e43fe44f3140b01f60e781f7f8
nts3112000 <xxxx9723@gmail.com>
2015-11-07 21:45:58 +0700

    /ff
    /ff

---
+++ MoonDraven/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace MoonDraven
+{
+    using System;
+
+    using LeagueSharp;
+    using LeagueSharp.Common;
+
+    internal class Program
+    {
+        #region Methods
+
+        private static void GameOnOnGameLoad(EventArgs args)
+        {
+            if (ObjectManager.Player.BaseSkinName == "Draven")
+            {
+                new MoonDraven().Load();
+            }
+        }
+
+        private static void Main(string[] args)
+        {
+            CustomEvents.Game.OnGameLoad += GameOnOnGameLoad;
+        }
+
+        #endregion
+    }
+}
